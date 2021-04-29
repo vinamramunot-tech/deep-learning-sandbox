880319fe64157605b5fbe1eb3ba7830a555cdf75
vinamramunot-tech <munov01@pfw.edu>
2021-04-28 22:29:56 -0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7c07df379c5e26bffc880474783cfaf70dd51ab2e25717af89939e5de49660fd
-size 616
+import subprocess
+from src import app, socketio, server_ip, server_port
+from src.services.server_protocol import get_https_auth_certs, DEFAULT_SERVER_IP
+import os
+
+if __name__ == '__main__':
+    subprocess.check_call(["pyclean", "."])
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    if (server_ip == DEFAULT_SERVER_IP):
+        socketio.run(app, host=server_ip, port=server_port, debug=True)
+    elif not get_https_auth_certs():
+        socketio.run(app, host=DEFAULT_SERVER_IP, port=server_port, debug=True)
+    else:
+        socketio.run(app, host=server_ip, port=server_port, ssl_context=get_https_auth_certs())
